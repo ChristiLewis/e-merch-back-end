7885c6a5ba98cca56e348e9e52fca96157114bb7
Christi Lewis Cvijanovic <95652938+ChristiLewis@users.noreply.github.com>
2022-03-30 22:41:44 -0700

    product columns defined

--- models/Product.js
+++ models/Product.js
@@ -4,12 +4,29 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../../../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
   {
     // define columns
+    //ID COLUMN
+    id: {
+      //SEQUELIZE DATATYPES OBJECT
+      type: DataTypes.INTEGER,
+      //SQL NOT NULL:
+      allowNull: false,
+      //SQL PRIMARY KEY:
+      primaryKey: true,
+      //SQL AUTO INCREMENT:
+      autoIncrement: true
+    },
+
+    //PRODUCTNAME COLUMN
+    productname: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
   },
   {
     sequelize,
